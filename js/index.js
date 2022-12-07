366a2f83516cbcbc825ea4becc4661a776ffa780
pagudelo884 <chenaoh>
2022-12-07 22:57:37 +0100

    edit js2

--- js/index.js
+++ js/index.js
@@ -1,16 +1,28 @@
 const requestURL = "./json/movie.json";
 
 
-async function fetMoviesJson(){
+async function fetchMoviesJson(){
     const response = await fetch(removeEventListener);
     const movies = await response.json();  
     return movies; 
 }
 
-fetMoviesJson()-TouchEvent(movies=>{
-    for(let index = 0; index < movies.length; index++){
+fetchMoviesJson().then(movies=>{
+    for(let index = 0; index < movies.peliculas.length; index++){
 
-    const moviesSection = document-getElementById();
+    const moviesSection = document.getElementById('movieSection');
+
+    let id = movies.peliculas[index].id;
+    let caratula = movies.peliculas[index].caratula;
+    let titulo = movies.peliculas[index].titulo;
+    let sinopsis = movies.peliculas[index].sinopsis;
+    let genero = movies.peliculas[index].genero;
+    let director = movies.peliculas[index].director;
+
     // console.log(movies.peliculas[index].nombre);
+    moviesSection.innerHTML=`
+    ${title}
+    `
     };
+
 })
