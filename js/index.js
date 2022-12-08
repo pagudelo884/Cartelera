e0806e0b1b36fac23d5f7a2f4fe62a3e0870eaf1
pagudelo884 <chenaoh>
2022-12-08 21:02:12 +0100

    edition js3

--- js/index.js
+++ js/index.js
@@ -1,28 +1,36 @@
-const requestURL = "./json/movie.json";
+const requestURL = `../json/peliculas.json`;
 
 
 async function fetchMoviesJson(){
-    const response = await fetch(removeEventListener);
+    const response = await fetch(requestURL);
     const movies = await response.json();  
     return movies; 
 }
 
-fetchMoviesJson().then(movies=>{
+fetchMoviesJson().then(movies =>{
     for(let index = 0; index < movies.peliculas.length; index++){
 
-    const moviesSection = document.getElementById('movieSection');
+        const moviesSection = document.getElementById('movieSection');
 
-    let id = movies.peliculas[index].id;
-    let caratula = movies.peliculas[index].caratula;
-    let titulo = movies.peliculas[index].titulo;
-    let sinopsis = movies.peliculas[index].sinopsis;
-    let genero = movies.peliculas[index].genero;
-    let director = movies.peliculas[index].director;
+        let id = movies.peliculas[index].id;
+        let caratula = movies.peliculas[index].caratula;
+        let titulo = movies.peliculas[index].titulo;
+        let sinopsis = movies.peliculas[index].sinopsis;
+        let genero = movies.peliculas[index].genero;
+        let director = movies.peliculas[index].director;
 
-    // console.log(movies.peliculas[index].nombre);
-    moviesSection.innerHTML=`
-    ${title}
-    `
+        // console.log(movies.peliculas[index]);
+
+        moviesSection.innerHTML  +=`
+            <div class="card" style="width: 18rem;">
+                <img src="${caratula}" class="card-img-top" alt="...">
+                <div class="card-body">
+                        <h3 class="card-title">${id}.${titulo}</h3>
+                        <h5 class="card-title">Director : ${director}</h5>
+                        <h5 class="card-title">Genero : ${genero}</h5>
+                </div>
+            </div>
+                `
     };
 
 })
