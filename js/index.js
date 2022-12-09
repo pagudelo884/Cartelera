f31b82bff2f3633d0c93146dedc252b7aad90080
pagudelo884 <chenaoh>
2022-12-09 09:08:55 +0100

    edition js4

--- js/index.js
+++ js/index.js
@@ -1,36 +1,35 @@
 const requestURL = `../json/peliculas.json`;
 
 
-async function fetchMoviesJson(){
-    const response = await fetch(requestURL);
-    const movies = await response.json();  
-    return movies; 
-}
+    async function fetchMoviesJson(){
+        const response = await fetch(requestURL);
+        const movies = await response.json();  
+        return movies; 
+    }
 
-fetchMoviesJson().then(movies =>{
-    for(let index = 0; index < movies.peliculas.length; index++){
+    fetchMoviesJson().then(movies =>{
+        for(let index = 0; index < movies.peliculas.length; index++){
 
-        const moviesSection = document.getElementById('movieSection');
+    const moviesSection = document.getElementById('movieSection');
 
         let id = movies.peliculas[index].id;
-        let caratula = movies.peliculas[index].caratula;
-        let titulo = movies.peliculas[index].titulo;
-        let sinopsis = movies.peliculas[index].sinopsis;
-        let genero = movies.peliculas[index].genero;
-        let director = movies.peliculas[index].director;
+        let cover = movies.peliculas[index].cover;
+        let song = movies.peliculas[index].song;
+        let genres = movies.peliculas[index].genres;
+        let artist = movies.peliculas[index].artist;
+        let album = movies.peliculas[index].album;
 
-        // console.log(movies.peliculas[index]);
+    console.log(movies.peliculas[index]);
 
         moviesSection.innerHTML  +=`
-            <div class="card" style="width: 18rem;">
-                <img src="${caratula}" class="card-img-top" alt="...">
+        <div class="card" style="width: 18rem;">
+            <img src="${cover}" class="card-img-top" alt="...">
                 <div class="card-body">
-                        <h3 class="card-title">${id}.${titulo}</h3>
-                        <h5 class="card-title">Director : ${director}</h5>
-                        <h5 class="card-title">Genero : ${genero}</h5>
+                    <h3 class="card-title">${id}.${artist}</h3>
+                    <h5 class="card-title">Director : ${genres}</h5>
+                    <h5 class="card-title">song : ${song}</h5>
                 </div>
-            </div>
-                `
+        </div>`
     };
 
 })
